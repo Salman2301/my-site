49201a00141fd79fc0546d62d02ea8022ad55633
Salman <salmanhammed77@gmail.com>
2023-04-22 02:03:30 +0530

    feat: content update whoami

--- src/lib/component/commands/output/WhoamiCommand.svelte
+++ src/lib/component/commands/output/WhoamiCommand.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-  import { icons } from "../../icon/map";
-
+  import { icons } from "@component/icon/map";
 
   export let links = [
     {
@@ -27,12 +26,67 @@
   <div class="full-border-style"></div>
 </div>
 
-<p>
-  Hello there, welcome to my site! I am <span class="text-hl1">Salman</span>, a full stack developer from India 🇮🇳. I have 4+ years experience in web development. I am passionate about web development and love to learn new things!
-</p>
-<br>
-<p>I am currently working as a <span class="text-hl1">full stack developer</span> at a startup.</p>
+<div class="body-content">
+  <div class="symbol">
+    <span>
+      <p>███████╗</p>
+      <p>██╔════╝</p>
+      <p>███████╗</p>
+      <p>╚════██║</p>
+      <p>███████║</p>
+      <p>╚══════╝</p>
+    </span>
+    <span>
+      <!-- a pathetic attempt not to let svelte trim the white space -->
+      <p>{""} █████╗ </p>
+      <p>██╔══██╗</p>
+      <p>███████║</p>
+      <p>██╔══██║</p>
+      <p>██║  ██║</p>
+      <p>╚═╝  ╚═╝</p>
+    </span>
+    <span>
+      <p>██╗     </p>
+      <p>██║     </p>
+      <p>██║     </p>
+      <p>██║     </p>
+      <p>███████╗</p>
+      <p>╚══════╝</p>
+    </span>
+    <span>
+      <p>███╗   ███╗</p>
+      <p>████╗ ████║</p>
+      <p>██╔████╔██║</p>
+      <p>██║╚██╔╝██║</p>
+      <p>██║ ╚═╝ ██║</p>
+      <p>╚═╝     ╚═╝</p>
+    </span>
+    <span>
+      <p>{""} █████╗ </p>
+      <p>██╔══██╗</p>
+      <p>███████║</p>
+      <p>██╔══██║</p>
+      <p>██║  ██║</p>
+      <p>╚═╝  ╚═╝</p>
+    </span>
+    <span>
+      <p>███╗   ██╗</p>
+      <p>████╗  ██║</p>
+      <p>██╔██╗ ██║</p>
+      <p>██║╚██╗██║</p>
+      <p>██║ ╚████║</p>
+      <p>╚═╝  ╚═══╝</p>
+    </span>
 
+
+  </div>
+  <p>
+    Hello there, welcome to my site! I am <span class="high-light">SALMAN</span>, a full stack developer from India 🇮🇳. I have 4+ years experience in web development. I am passionate about web development and love to learn new things!
+  </p>
+  <br>
+  <p>I am currently working as a <span class="high-light">full stack developer</span> at a startup.</p>
+
+</div>
 
 <p class="header-label text-t1">Social links:</p>
 <ul>
@@ -95,5 +149,80 @@
     height: 30px;
     @apply flex items-center justify-center;
     @apply font-bold;
+  }
+
+  .high-light {
+    @apply text-hl1 uppercase;
+  }
+
+  .symbol {
+    font-family: monospace;
+    @apply flex justify-center gap-1;
+  }
+  .body-content {
+    width: 100%;
+  }
+  span {
+    font-family: monospace;
+  }
+
+  .symbol span {
+    @apply text-t1;
+    animation: TextColor 1.2s ease infinite;
+  }
+
+  span p {
+    white-space: pre;
+  }
+
+  .symbol span:nth-child(1) {
+    animation-delay: 0.1s;
+  }
+  .symbol span:nth-child(2) {
+    animation-delay: 0.2s;
+  }
+  .symbol span:nth-child(3) {
+    animation-delay: 0.3s;
+  }
+  .symbol span:nth-child(4) {
+    animation-delay: 0.4s;
+  }
+  .symbol span:nth-child(5) {
+    animation-delay: 0.5s;
+  }
+  .symbol span:nth-child(6) {
+    animation-delay: 0.6s;
+  }
+  .symbol span:nth-child(7) {
+    animation-delay: 0.7s;
+  }
+  .symbol span:nth-child(8) {
+    animation-delay: 0.8s;
+  }
+  .symbol span:nth-child(9) {
+    animation-delay: 0.9s;
+  }
+  .symbol span:nth-child(10) {
+    animation-delay: 1s;
+  }
+
+  @keyframes TextColor { 
+    0% {
+      transform: scale(1.05);
+      margin-top: 0px;
+      margin-bottom: 10px;
+    }
+    60% {
+      color: var(--hl1);
+    }
+    85% {
+      color: var(--hl1);
+    }
+    100% {
+      transform: scale(1);
+      margin-top: 10px;
+      margin-bottom: 0px;
+      color: var(--t2);
+    }
   }
 </style>
